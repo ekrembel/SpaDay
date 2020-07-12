9eb133031bc2ae725b132ff5864b4138f3f34ee9
Ekrem Bel <ekrem@Ekrems-MacBook-Pro-2.local>
2020-07-11 20:26:42 -0500

    Done with studio14

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(User newUser, string confirmPassword)
+        {
+            ViewBag.UserName = newUser.UserName;
+            ViewBag.Date = newUser.Date;
+            ViewBag.message = "Password does not match!";
+            ViewBag.username = newUser.UserName;
+            ViewBag.email = newUser.Email;
+            if (newUser.Password == confirmPassword)
+            {
+                
+                return View("Index");
+            }
+
+            return View();
+            
+        }
+    }
+
+
+}
---
+++ SpaDay/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SpaDay.Models
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime Date { get; set; }
+
+        public List<User> Users = new List<User>();
+
+        public User()
+        {
+            Date = DateTime.Now;
+        }
+
+
+    }
+}
